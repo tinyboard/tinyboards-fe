cefd54254488f0604d7c83e2a6db5c48a2e109c9
drake <drake@noreply.git.tinyboards.net>
2022-11-12 11:55:24 -0500

    indentation

--- layouts/feed.vue
+++ layouts/feed.vue
@@ -5,13 +5,13 @@
 		<!-- Filters -->
 		<div class="flex justify-between mb-4 px-4 sm:px-0">
 			<NuxtLink to="/submit" class="ml-auto button primary flex items-center">
-		        <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-				   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-				   <path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4"></path>
-				   <line x1="13.5" y1="6.5" x2="17.5" y2="10.5"></line>
+				<svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+					<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+					<path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4"></path>
+					<line x1="13.5" y1="6.5" x2="17.5" y2="10.5"></line>
 				</svg>
-		        <span>New post</span>
-		    </NuxtLink>
+				<span>New post</span>
+			</NuxtLink>
 		</div>
 		<!-- Loading State -->
 		<div v-if="pending">
@@ -35,7 +35,7 @@
 <script setup>
 	import { getListing } from '@/composables/posts';
 
-  let sort = useRoute().params.sort ?? 'new';
+	let sort = useRoute().params.sort ?? 'new';
 
 	let { items: posts, paginate, pending, error, refresh } = await getListing({
 		sort: sort,
