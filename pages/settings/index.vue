aabb46614c0c5a818347ba1c430be1e9782f0938
drake <drake@noreply.git.tinyboards.net>
2022-11-27 12:38:16 -0500

    functional banner file input

--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -16,19 +16,17 @@
 							<label class="text-base font-bold leading-6 text-gray-900">Avatar</label>
 						</div>
 						<!-- Inputs -->
-						<div class="mt-5 md:col-span-2 md:mt-0">
-							<div class="mt-1 flex items-center">
-								<img v-if="settings.avatar" :src="settings.avatar" class="w-20 h-20 object-cover p-0.5 border bg-white"/>
-								<div v-else class="w-20 h-20 rounded-sm border border-gray-300 border-dashed"></div>
-								<div class="ml-5">
-									<label for="avatar-upload" class="inline-block button white cursor-pointer">
-										Change
-									</label>
-									<input id="avatar-upload" type="file" class="hidden" accept="image/png, image/jpeg, image/gif" @change="onFileChange"/>
-									<small class="block mt-2 text-gray-400">
-										Max file size allowed is 1mb. Supports PNG, JPG and GIF.
-									</small>
-								</div>
+						<div class="mt-5 md:col-span-2 md:mt-0 flex items-center">
+							<img v-if="settings.avatar" :src="settings.avatar" class="w-20 h-20 object-cover p-0.5 border bg-white"/>
+							<div v-else class="w-20 h-20 rounded-md border border-gray-300 border-dashed"></div>
+							<div class="ml-5">
+								<label for="avatar-upload" class="inline-block button white cursor-pointer">
+									Change
+								</label>
+								<input id="avatar-upload" type="file" class="hidden" accept="image/png, image/jpeg, image/gif" @change="onFileChange($event,'avatar')"/>
+								<small class="block mt-2 text-gray-400">
+									PNG, JPG and GIF up to 1MB.
+								</small>
 							</div>
 						</div>
 					</div>
@@ -39,28 +37,18 @@
 							<label class="text-base font-bold leading-6 text-gray-900">Banner</label>
 						</div>
 						<!-- Inputs -->
-						<div class="mt-5 md:col-span-2 md:mt-0">
-							<div class="mt-1 flex justify-center rounded-md border border-dashed border-gray-300 px-6 pt-5 pb-6">
-								<div class="space-y-1 text-center">
-									<svg xmlns="http://www.w3.org/2000/svg" class="mx-auto h-10 w-10 text-gray-400" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-										<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-										<path d="M15 8h.01"></path>
-										<path d="M12 20h-5a3 3 0 0 1 -3 -3v-10a3 3 0 0 1 3 -3h10a3 3 0 0 1 3 3v5"></path>
-										<path d="M4 15l4 -4c.928 -.893 2.072 -.893 3 0l4 4"></path>
-										<path d="M14 14l1 -1c.617 -.593 1.328 -.793 2.009 -.598"></path>
-										<path d="M16 19h6"></path>
-										<path d="M19 16v6"></path>
-									</svg>
-									<div class="flex text-sm text-gray-600">
-										<label for="banner-upload" class="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500">
-											<span>Upload a file</span>
-											<input id="banner-upload" name="banner-upload" type="file" class="sr-only" />
-										</label>
-										<p class="pl-1">or drag and drop</p>
-									</div>
-									<p class="text-xs text-gray-500">PNG, JPG, GIF up to 1MB</p>
+						<div class="mt-5 md:col-span-2 md:mt-0 flex flex-col">
+								<img v-if="settings.banner" :src="settings.banner" class="w-full h-36 object-cover p-0.5 border bg-white"/>
+								<div v-else class="w-full h-36 rounded-md border border-gray-300 border-dashed"></div>
+								<div class="mt-5">
+									<label for="banner-upload" class="inline-block button white cursor-pointer">
+										Change
+									</label>
+									<input id="banner-upload" type="file" class="hidden" accept="image/png, image/jpeg, image/gif" @change="onFileChange($event)"/>
+									<small class="block mt-2 text-gray-400">
+										PNG, JPG and GIF up to 1MB. Recommended 1390x192 pixels.
+									</small>
 								</div>
-							</div>
 						</div>
 					</div>
 					<!-- Display Name -->
@@ -85,9 +73,7 @@
 						</div>
 						<!-- Inputs -->
 						<div class="mt-5 md:col-span-2 md:mt-0">
-							<div class="mt-1">
-								<textarea id="biography" name="biography" rows="4" class="mt-1 block w-full rounded-md border-gray-200 bg-gray-100 shadow-inner-xs focus:bg-white focus:border-primary focus:ring-primary text-base" placeholder="A stranger surfing this World Wide Web." v-model="settings.bio"/>
-							</div>
+							<textarea id="biography" name="biography" rows="4" class="mt-1 block w-full rounded-md border-gray-200 bg-gray-100 shadow-inner-xs focus:bg-white focus:border-primary focus:ring-primary text-base" placeholder="A stranger surfing this World Wide Web." v-model="settings.bio"/>
 							<p class="mt-2 text-sm text-gray-500">Brief description about yourself. Markdown supported.</p>
 						</div>
 					</div>
@@ -119,11 +105,16 @@
 	// File inputs.
 	const url = ref('https://i.imgur.com/nzY5zAg.jpg');
 
-	const onFileChange = (e) => {
+	const onFileChange = (e,type) => {
+		console.log(type)
 		const file = e.target.files[0];
 		const reader = new FileReader();
 		reader.onloadend = () => {
-			settings.value.avatar = reader.result;
+			if (type === 'avatar') {
+				settings.value.avatar = reader.result;
+			} else {
+				settings.value.banner = reader.result;
+			}
 		};
 		reader.readAsDataURL(file);
 	};
