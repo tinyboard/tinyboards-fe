785fc9b6b5ddfb0f49a647347bbd06c014d27a6f
drake <drake@noreply.git.tinyboards.net>
2022-12-07 20:32:51 -0500

    fix padding

--- components/navigation/NavbarSub.vue
+++ components/navigation/NavbarSub.vue
@@ -1,10 +1,10 @@
 <template>
 	<div class="sm:fixed w-full bg-gray-100 border-b dark:bg-secondary dark:border-white/10 z-40">
-		<div class="mx-auto max-w-8xl flex items-center justify-between px-4 sm:px-6">
+		<div class="mx-auto max-w-8xl flex items-center justify-between px-2.5 sm:px-6">
 			<ul class="flex items-center overflow-x-auto scrollbar-hidden p-[1px] m-[-1px]">
 				<li class="text-sm leading-6 flex-shrink-0" v-for="link in links" :key="link.name">
 					<NuxtLink v-if="!link.isExternal" :to="link.href" custom v-slot="{ href, navigate, isActive }">
-						<a :href="href" :target="link.target" @click="navigate" :class="[isActive && !disableActiveClass ? 'text-primary shadow-[0_1px_0_rgba(var(--color-primary))]' : 'text-gray-500 hover:text-primary', 'px-2.5 sm:px-4 py-2.5 inline-block']">
+						<a :href="href" :target="link.target" @click="navigate" :class="[isActive && !disableActiveClass ? 'text-primary shadow-[0_1px_0_rgba(var(--color-primary))]' : 'text-gray-500 hover:text-primary', 'px-4 py-2.5 inline-block']">
 							{{ link.name }}
 						</a>
 					</NuxtLink>
