6487de9252d95753e2116db2a9fa929085d51b0d
drake <drake@noreply.git.tinyboards.net>
2022-12-27 13:28:22 -0500

    remove bloat

--- composables/user.js
+++ composables/user.js
@@ -1,9 +1,6 @@
-//import { $fetch } from "ohmyfetch";
-//import { useFetch } from "nuxt";
 import { useApi } from "@/composables/api";
 
 export async function useFetchUser(username) {
-  //const user = await $fetch(`https://jsonplaceholder.typicode.com/users/?username=${username.value}`)
   return useApi(`/user/${username}`, {
     key: `user_${username}`,
   });
