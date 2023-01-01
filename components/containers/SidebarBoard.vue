6c67c8a98c261a70409f83fd0282248db7b04c2d
Verstopfung <throwaway.546372819@protonmail.com>
2023-01-01 03:43:48 +0100

    create post button

--- components/containers/SidebarBoard.vue
+++ components/containers/SidebarBoard.vue
@@ -2,6 +2,16 @@
     <div
       class="w-[290px] hidden xl:flex flex-col flex-shrink-0 space-y-6 text-base"
     >
+    <router-link :to="`/b/${board.name}/submit`" custom v-slot="{ navigate }">
+		<button class="button primary w-full text-left" @click="navigate" @keypress.enter="navigate" role="link">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4"></path>
+                <line x1="13.5" y1="6.5" x2="17.5" y2="10.5"></line>
+            </svg>
+			Create Post in /b/{{ board.name }}
+		</button>
+	  </router-link>
       <div>
         <h2 class="font-bold leading-5 text-base mb-3 px-2 pb-1 border-b">
             <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
