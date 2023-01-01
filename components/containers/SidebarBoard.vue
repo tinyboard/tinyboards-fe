f406febe53e714cff20f47044de5bd3371189985
drake <drake@noreply.git.tinyboards.net>
2022-12-31 21:58:46 -0500

    style

--- components/containers/SidebarBoard.vue
+++ components/containers/SidebarBoard.vue
@@ -25,28 +25,28 @@
         <h2 class="font-bold leading-5 text-base mb-1 pb-1 border-b">
             <span>Moderation</span>
         </h2>
-        <ul class="space-y-1">
+        <ul class="flex flex-col space-y-2 py-2">
             <li>
-                <button class="w-full bg-gray-300 bg-opacity-0 hover:bg-opacity-40 p-2 rounded text-left">
+                <NuxtLink to="/" class="flex items-center">
                     <svg class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                         <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z"></path>
                         <circle cx="12" cy="12" r="3"></circle>
                     </svg>
                     <span>Overview</span>
-                </button>
+                </NuxtLink>
             </li>
             <li>
-                <button class="w-full bg-gray-300 bg-opacity-0 hover:bg-opacity-40 p-2 rounded text-left">
+                <NuxtLink to="/" class="flex items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                         <path d="M5 14h14l-4.5 -4.5l4.5 -4.5h-14v16"></path>
                     </svg>
                     <span>Mod Queue</span>
-                </button>
+                </NuxtLink>
             </li>
             <li>
-                <button class="w-full bg-gray-300 bg-opacity-0 hover:bg-opacity-40 p-2 rounded text-left">
+                <NuxtLink to="/" class="flex items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                         <path d="M14.274 10.291a4 4 0 1 0 -5.554 -5.58m-.548 3.453a4.01 4.01 0 0 0 2.62 2.65"></path>
@@ -54,10 +54,10 @@
                         <line x1="3" y1="3" x2="21" y2="21"></line>
                     </svg>
                     <span>Banned Users</span>
-                </button>
+                </NuxtLink>
             </li>
             <li>
-                <button class="w-full bg-gray-300 bg-opacity-0 hover:bg-opacity-40 p-2 rounded text-left">
+                <NuxtLink to="/" class="flex items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                         <path d="M12 21a9 9 0 0 1 0 -18c4.97 0 9 3.582 9 8c0 1.06 -.474 2.078 -1.318 2.828c-.844 .75 -1.989 1.172 -3.182 1.172h-2.5a2 2 0 0 0 -1 3.75a1.3 1.3 0 0 1 -1 2.25"></path>
@@ -66,11 +66,11 @@
                         <circle cx="16.5" cy="10.5" r="1"></circle>
                     </svg>
                     <span>Appearance</span>
-                </button>
+                </NuxtLink>
             </li>
             <li>
-                <button class="w-full bg-gray-300 bg-opacity-0 hover:bg-opacity-40 p-2 rounded text-left">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <NuxtLink to="/" class="flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                         <line x1="9" y1="6" x2="20" y2="6"></line>
                         <line x1="9" y1="12" x2="20" y2="12"></line>
@@ -80,26 +80,26 @@
                         <line x1="5" y1="18" x2="5" y2="18.01"></line>
                     </svg>
                     <span>Moderation Log</span>
-                </button>
+                </NuxtLink>
             </li>
         </ul>
       </div>
       <div>
-        <h2 class="font-bold leading-5 text-base mb-1 pb-1 border-b">
+        <h2 class="font-bold leading-5 text-base pb-1 border-b">
             <span>Admin Tools</span>
         </h2>
-        <ul class="space-y-1">
+        <ul class="flex flex-col space-y-2 mt-4">
             <li>
-                <NuxtLink class="w-full bg-gray-300 bg-opacity-0 hover:bg-opacity-40 p-2 rounded text-left">
+                <button class="flex items-center button green w-full">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                         <path d="M12 3a12 12 0 0 0 8.5 3a12 12 0 0 1 -8.5 15a12 12 0 0 1 -8.5 -15a12 12 0 0 0 8.5 -3"></path>
                     </svg>
                     <span>Mod Self</span>
-                </NuxtLink>
+                </button>
             </li>
             <li>
-                <button class="w-full bg-gray-300 bg-opacity-0 hover:bg-opacity-40 p-2 rounded text-left">
+                <button class="flex items-center button yellow w-full">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                         <path d="M5 11a7 7 0 0 1 14 0v7a1.78 1.78 0 0 1 -3.1 1.4a1.65 1.65 0 0 0 -2.6 0a1.65 1.65 0 0 1 -2.6 0a1.65 1.65 0 0 0 -2.6 0a1.78 1.78 0 0 1 -3.1 -1.4v-7"></path>
@@ -111,8 +111,8 @@
                 </button>
             </li>
             <li>
-                <button class="w-full bg-gray-300 bg-opacity-0 hover:bg-opacity-40 p-2 rounded text-left text-red-500">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-gavel" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <button class="flex items-center button red w-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                         <path d="M13 10l7.383 7.418c.823 .82 .823 2.148 0 2.967a2.11 2.11 0 0 1 -2.976 0l-7.407 -7.385"></path>
                         <path d="M6 9l4 4"></path>
