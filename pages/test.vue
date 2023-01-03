942deae55ec79f350818c62184071b1cd2ebc759
drake <drake@noreply.git.tinyboards.net>
2023-01-03 09:45:07 -0500

    test page

---
+++ pages/test.vue
@@ -0,0 +1,16 @@
+<template>
+      <div class="py-24">
+            {{ text }}
+      </div>
+</template>
+
+<script setup>
+      const text = ref(null);
+
+      useFetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(({ data, error }) => {
+            if (data.value) {
+                  text.value = data.value
+            }
+      });
+</script>
