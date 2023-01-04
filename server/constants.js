ea1e0baa39fc7c8f4c4191be3aecba04af32a8a4
kroner <kronusdev@protonmail.com>
2023-01-03 20:53:54 -0600

    REEEEEEE

--- server/constants.js
+++ server/constants.js
@@ -1,2 +1,3 @@
 // export const baseURL = "http://tinyboards.test:1236/api/v1";
-export const baseURL = "https://the.tree-house.cloud/api/v1";
+//export const baseURL = "https://the.tree-house.cloud/api/v1";
+export const baseURL = "https://forum.porpl.net";
