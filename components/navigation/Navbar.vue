f65edd9d613acf7a1de1f1bf3730accf5cfd0832
drake <drake@noreply.git.tinyboards.net>
2023-01-05 18:23:27 -0500

    mobile menu

--- components/navigation/Navbar.vue
+++ components/navigation/Navbar.vue
@@ -24,57 +24,62 @@
 					</NuxtLink>
 				</div>
 				<div class="-mr-1 flex space-x-1 md:hidden">
-					<!-- Search button -->
-					<button class="inline-flex items-center justify-center p-1 text-gray-600 dark:text-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" @click="searchDrawer">
-						<span class="sr-only">Search</span>
-						<!-- Menu Icon -->
-						<i class="far fa-search fa-fw fa-lg"></i>
-					</button>
 					<!-- Mobile menu button -->
-					<button class="inline-flex items-center justify-center p-1 text-gray-600 dark:text-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" @click="drawer">
+					<button class="inline-flex items-center justify-center p-1 text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" @click="toggleDrawer">
 						<span class="sr-only">Open main menu</span>
 						<!-- Menu Icon -->
-						<i class="far fa-bars fa-fw fa-lg"></i>
+						<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+						   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+						   <line x1="4" y1="6" x2="20" y2="6"></line>
+						   <line x1="4" y1="12" x2="20" y2="12"></line>
+						   <line x1="4" y1="18" x2="20" y2="18"></line>
+						</svg>
 					</button>
 				</div>
 				<!-- Mobile Menu -->
 				<transition enter-class="opacity-0" enter-active-class="ease-out transition-all duration-100" enter-to-class="opacity-200" leave-class="opacity-100" leave-active-class="ease-out transition-all duration-200" leave-to-class="opacity-0">
-					<div @keydown.esc="isOpen = false" v-show="isOpen" class="z-10 fixed inset-0 transition-opacity">
-						<div @click="isOpen = false" class="absolute inset-0 bg-gray-900 opacity-80" tabindex="0"></div>
-					</div>
+					<div @click="isOpen = false" @keydown.esc="isOpen = false" v-show="isOpen" class="fixed inset-0 bg-black/80"></div>
 				</transition>
 				<aside class="transform top-0 right-0 w-5/6 bg-white dark:bg-gray-800 fixed h-full py-3 overflow-y-auto ease-in-out transition-all duration-200 z-75" :class="isOpen ? 'translate-x-0' : 'translate-x-full'">
-					<div class="absolute top-4 left-3">
-						<button class="inline-flex items-center justify-center p-1 text-gray-600 dark:text-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" @click="drawer">
+					<div class="absolute top-3 left-3">
+						<button class="inline-flex items-center justify-center p-1 text-gray-700 dark:text-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" @click="toggleDrawer">
 							<span class="sr-only">Close menu</span>
-							<!-- Menu Icon -->
-							<i class="far fa-long-arrow-left fa-fw fa-lg"></i>
+							<!-- Arrow Left Icon -->
+							<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+							   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+							   <line x1="5" y1="12" x2="19" y2="12"></line>
+							   <line x1="5" y1="12" x2="9" y2="16"></line>
+							   <line x1="5" y1="12" x2="9" y2="8"></line>
+							</svg>
 						</button>
 					</div>
 					<div class="flex flex-col items-center mt-8 px-4 py-2">
-						<NuxtLink to="/login" class="button outlinePrimary w-full">
-								Sign in
-						</NuxtLink>
-						<NuxtLink to="/register" class="button primary mt-2 w-full">
+						<NuxtLink to="/register" class="button primary w-full">
 								Sign up
 						</NuxtLink>
+						<NuxtLink to="/login" class="button white mt-2 w-full">
+								Sign in
+						</NuxtLink>
 					</div>
-					<router-link to="/following" class="block px-4 py-2 text-lg text-gray-700 dark:text-gray-200" @click="isOpen = false">
-						<i class="far fa-info-circle text-center fa-fw mr-3"></i><span>About</span>
-					</router-link>
-					<router-link to="/guilds" class="block px-4 py-2 text-lg text-gray-700 dark:text-gray-200" @click="isOpen = false">
-						<i class="far fa-scroll-old text-center fa-fw mr-3"></i><span>Rules</span>
-					</router-link>
-					<router-link to="/saved" class="block px-4 py-2 text-lg text-gray-700 dark:text-gray-200" @click="isOpen = false">
-						<i class="fab fa-github text-center fa-fw mr-3"></i><span>Source Code</span>
-					</router-link>
-					<router-link to="/settings" class="block px-4 py-2 text-lg text-gray-700 dark:text-gray-200" @click="isOpen = false">
-						<i class="fab fa-discord text-center fa-fw mr-3"></i><span>Discord</span>
-					</router-link>
-					<button class="flex items-center px-4 py-2 text-lg text-gray-700 dark:text-gray-200" @click="dark = !dark">
-						<i class="far text-center fa-fw mr-3" :class="dark ? 'fa-sunglasses' : 'fa-moon'"></i>
-						<span>{{ dark ? 'Light Theme' : 'Dark Theme'}}</span>
-					</button>
+					<NuxtLink to="/help/faq" class="flex items-center px-4 py-2 text-gray-700 dark:text-gray-200" @click="isOpen = false">
+						<svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+						   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+						   <path d="M12 8h.01"></path>
+						   <path d="M11 12h1v4h1"></path>
+						   <path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z"></path>
+						</svg>
+						<span>About</span>
+					</NuxtLink>
+					<NuxtLink to="/help/rules" class="flex items-center px-4 py-2 text-gray-700 dark:text-gray-200" @click="isOpen = false">
+						<svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+						   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+						   <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
+						   <path d="M5 8v-3a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2h-5"></path>
+						   <circle cx="6" cy="14" r="3"></circle>
+						   <path d="M4.5 17l-1.5 5l3 -1.5l3 1.5l-1.5 -5"></path>
+						</svg>
+						<span>Rules</span>
+					</NuxtLink>
 				</aside>
 			</div>
 		</div>
@@ -85,7 +90,6 @@
 	import { useSiteStore } from '@/stores/StoreSite.js'
 
 	const site = useSiteStore()
-	const isOpen = false;
 	const dark = false;
 
 	// Yellow Text
@@ -106,4 +110,11 @@
 	if (typeof window === 'undefined') {
 		selectedText.value = shuffle(yellowText)[0] ?? 'It\'s Rails!';
 	};
+
+	// Mobile menu
+	const isOpen = ref(false);
+
+	const toggleDrawer = () => {
+		isOpen.value = !isOpen.value;
+	}
 </script>
