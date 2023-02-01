5183ccfd0233988e41931be266dc1850c0e2df97
sic <sicxnull@protonmail.com>
2023-01-31 21:13:25 -0500

    env baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,26 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+// nuxt.config.ts
 import { defineNuxtConfig } from "nuxt/config";
+import dotenv from 'dotenv'
 
 const tls = require('tls');
 tls.DEFAULT_ECDH_CURVE = 'auto';
 
+dotenv.config();
+
 export default defineNuxtConfig({
-	alias: {
+    alias: {
         pinia: '/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs',
     },
     css: ["~/assets/css/main.css"],
     modern: true,
     modules: [
-      "@pinia/nuxt",
-      "@nuxtjs/tailwindcss"
+        "@pinia/nuxt",
+        "@nuxtjs/tailwindcss"
     ],
+    env: {
+        BASE_URL: process.env.TB_URL,
+    },
     routeRules: {
         // Static page generated on-demand once
         '/help/**': { static: true },
