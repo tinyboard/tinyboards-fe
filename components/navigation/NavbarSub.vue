000965271063d985d8f3d9b20e89acc1b2124cb9
githubcom <135359235+githubcom3@users.noreply.github.com>
2023-06-08 12:28:28 -0400

    css selectors

--- components/navigation/NavbarSub.vue
+++ components/navigation/NavbarSub.vue
@@ -1,8 +1,8 @@
 <template>
-	<div class="sm:fixed w-full h-12 bg-gray-100 border-b dark:bg-secondary dark:border-white/10 sm:z-30">
+	<div id="sub-header" class="sm:fixed w-full h-12 bg-gray-100 border-b dark:bg-secondary dark:border-white/10 sm:z-30">
 		<div class="mx-auto max-w-8xl h-full flex items-center justify-between px-2.5 sm:px-6">
-			<ul class="flex items-center h-full overflow-x-auto scrollbar-hidden box-content p-[1px] m-[-1px]">
-				<li class="flex-shrink-0 h-full text-sm leading-6" v-for="link in links" :key="link.name">
+			<ul class="menu flex items-center h-full overflow-x-auto scrollbar-hidden box-content p-[1px] m-[-1px]">
+				<li class="menu-item flex-shrink-0 h-full text-sm leading-6" v-for="link in links" :key="link.name">
 					<NuxtLink :external="link.isExternal" :to="link.href" custom v-slot="{ href, navigate, isActive }">
 						<a :href="href" :target="link.target" @click="navigate" :class="[isActive && !disableActiveClass ? 'text-primary shadow-[0_1px_0_rgba(var(--color-primary))]' : 'text-gray-500 hover:text-primary', 'inline-flex items-center h-full px-4']">
 							{{ link.name }}
