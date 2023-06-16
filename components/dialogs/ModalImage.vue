f084809b1b560b2a3fa4e5460a578211de0153b1
githubcom <135359235+githubcom3@users.noreply.github.com>
2023-06-16 15:13:56 -0400

    dark mode

--- components/dialogs/ModalImage.vue
+++ components/dialogs/ModalImage.vue
@@ -11,8 +11,8 @@
           <TransitionChild as="template" enter="duration-300 ease-[cubic-bezier(.2,0,0,1.4)]" enter-from="opacity-0 scale-90" enter-to="opacity-100 scale-100" leave="duration-200 ease-[cubic-bezier(.2,0,0,1.4)]" leave-from="opacity-100 scale-100" leave-to="opacity-0 scale-90">
             <DialogPanel class="w-full max-w-md transform overflow-hidden transition-all">
               <!-- Body -->
-              <img :src="options.img.src" :alt="options.img.alt" class="w-full h-full shadow-xl p-2.5 border bg-white"/>
-              <a :href="options.img.src" target="_blank" class="inline-block mt-2 text-gray-300 text-sm font-medium hover:underline focus:outline-none">
+              <img :src="options.img.src" :alt="options.img.alt" class="w-full h-full shadow-xl p-2.5 border dark:border-gray-800 bg-white dark:bg-gray-900"/>
+              <a :href="options.img.src" target="_blank" class="inline-block mt-2 text-gray-300 dark:text-gray-400 text-sm font-medium hover:underline focus:outline-none">
                   Open in new tab
               </a>
             </DialogPanel>
