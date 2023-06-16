7563719780921f9e0f8b98ca2cd141e799c0bb49
githubcom <135359235+githubcom3@users.noreply.github.com>
2023-06-16 14:01:45 -0400

    404 space

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,5 +1,5 @@
 <template>
-	<div id="error" class="relative flex flex-1 flex-col overflow-hidden bg-cover bg-center">
+	<div id="error" class="relative flex flex-1 flex-col overflow-hidden">
 		<nav class="fixed w-full z-50">
 			<div class="mx-auto max-w-8xl px-4 sm:px-6">
 				<h1 class="sr-only">{{ site.name }}</h1>
@@ -30,6 +30,7 @@
 			</div>
 		</footer>
 		<DialogsToastList />
+		<canvas id="space" class="fixed inset-0 z-[-1] pointer-events-none"></canvas>
 	</div>
 </template>
 
@@ -48,27 +49,98 @@
 	const site = useSiteStore();
 
 	const links = [
-	{ name: 'About', href: '/help/about' },
-	{ name: 'FAQ', href: '/help' },
-	{ name: 'House Rules', href: '/help/rules' },
-	];
-</script>
+		{ name: 'About', href: '/help/about' },
+		{ name: 'FAQ', href: '/help' },
+		{ name: 'House Rules', href: '/help/rules' },
+		];
 
-<style scoped>
-	#error {
-		background-image: url('https://i.imgur.com/1rGd3Ki.gif');
-		background-position: center;
-		background-repeat: no-repeat;
-		background-size: cover;
+	// https://codepen.io/hfer/pen/VQyqLg
+	if (process.client) {
+		window.requestAnimFrame = (function(){
+			return  window.requestAnimationFrame
+		})();
+
+		var canvas = document.getElementById("space");
+		var c = canvas.getContext("2d");
+
+		var numStars = 900;
+		var radius = '0.'+Math.floor(Math.random() * 9) + 1;
+		var focalLength = canvas.width *2;
+
+		var centerX, centerY;
+
+		var stars = [], star;
+		var i;
+
+		var animate = true;
+
+		initStars();
 	}
-	#error::before {
-		content: '';
-		position: fixed;
-		pointer-events: none;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-		background-color: rgba(0,0,0,0.9);
+
+	function executeFrame(){
+		if (animate) {
+			requestAnimFrame(executeFrame);
+			moveStars();
+			drawStars();
+		}
 	}
-</style>
+
+	function initStars(){
+		centerX = canvas.width / 2;
+		centerY = canvas.height / 2;
+
+		stars = [];
+		for(i = 0; i < numStars; i++){
+			star = {
+				x: Math.random() * canvas.width,
+				y: Math.random() * canvas.height,
+				z: Math.random() * canvas.width,
+				o: `0.${Math.floor(Math.random() * 99) + 1}`
+			};
+			stars.push(star);
+		}
+	}
+
+	function moveStars(){
+		for(i = 0; i < numStars; i++){
+			star = stars[i];
+			star.z--;
+
+			if(star.z <= 0){
+				star.z = canvas.width;
+			}
+		}
+	}
+
+	function drawStars() {
+		var pixelX, pixelY, pixelRadius;
+
+  		// Resize to screen
+		if(canvas.width != window.innerWidth || canvas.width != window.innerWidth){
+			canvas.width = window.innerWidth;
+			canvas.height = window.innerHeight;
+			initStars();
+		}
+
+		c.fillStyle = "rgba(0,0,0,1)";
+		c.fillRect(0, 0, canvas.width, canvas.height);
+
+		c.fillStyle = `rgba(255, 255, 255, ${radius}`;
+		for(i = 0; i < numStars; i++){
+			star = stars[i];
+
+			pixelX = (star.x - centerX) * (focalLength / star.z);
+			pixelX += centerX;
+			pixelY = (star.y - centerY) * (focalLength / star.z);
+			pixelY += centerY;
+			pixelRadius = 1 * (focalLength / star.z);
+
+			c.fillRect(pixelX, pixelY, pixelRadius, pixelRadius);
+			c.fillStyle = `rgba(255, 255, 255, ${star.o})`
+		}
+	}
+
+	if (process.client) {
+		executeFrame();
+	}
+</script>
