abec4f39728fb98a2ad8fdbdc980db50a2669087
githubcom <135359235+githubcom3@users.noreply.github.com>
2023-07-07 16:01:20 -0400

    debug

---
+++ layouts/debug.vue
@@ -0,0 +1,13 @@
+<template>
+	<main id="layout-debug" class="flex flex-col pt-12 sm:pt-14">
+		<section>
+			<slot/>
+		</section>
+	</main>
+</template>
+
+<script setup>
+	useHead({
+		title: `Tinyboards | debug`
+	});
+</script>
---
+++ pages/debug.vue
@@ -0,0 +1,13 @@
+<template>
+      <NuxtLayout name="debug">
+            {{ JSON.stringify(v) }}
+      </NuxtLayout>
+</template>
+
+<script setup>
+      import { useLoggedInUser } from '@/stores/StoreAuth';
+
+      const userStore = useLoggedInUser();
+
+      const v = userStore.user;
+</script>
