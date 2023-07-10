174eea0e4e0a44e4ae55561c8c3e2564910078b3
i-dont-know11 <129621627+i-dont-know11@users.noreply.github.com>
2023-07-10 15:58:15 +0200

    report list styling "improvements"

--- components/cards/Reports.vue
+++ components/cards/Reports.vue
@@ -1,16 +1,16 @@
 <template>
-	<div class="bg-yellow-500 p-2 rounded">
-		<div class="text-white flex flex-row justify-between">
-			<div class="flex flex-row font-bold">
-				<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-flag-3 my-[-1.5px] mr-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+	<div class="bg-yellow-400 bg-opacity-60 p-2 rounded">
+		<div class="text-gray-600 text-sm flex flex-row justify-between">
+			<div class="flex flex-row font-bold text-orange-700">
+				<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-flag-3-filled my-[-3px] mr-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
 				   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-				   <path d="M5 14h14l-4.5 -4.5l4.5 -4.5h-14v16"></path>
+				   <path d="M19 4c.852 0 1.297 .986 .783 1.623l-.076 .084l-3.792 3.793l3.792 3.793c.603 .602 .22 1.614 -.593 1.701l-.114 .006h-13v6a1 1 0 0 1 -.883 .993l-.117 .007a1 1 0 0 1 -.993 -.883l-.007 -.117v-16a1 1 0 0 1 .883 -.993l.117 -.007h14z" stroke-width="0" fill="currentColor"></path>
 				</svg>
 				User Reports
 			</div>
 			<button @click="toggleOpen" class="flex flex-row">
 				{{ open ? 'Close' : 'Expand' }}
-				<svg v-if="open" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrows-diagonal-minimize-2 my-[-1.5px] ml-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+				<svg v-if="open" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrows-diagonal-minimize-2 my-[-3px] ml-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
 				   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
 				   <path d="M18 10h-4v-4"></path>
 				   <path d="M20 4l-6 6"></path>
@@ -34,7 +34,7 @@
 				</p>
 			</div>
 		</div>
-		<p v-if="open" class="text-sm text-gray-100 pt-1 px-2">Approving or removing this post will automatically resolve these reports.</p>
+		<p v-if="open" class="text-sm text-gray-600 pt-1 px-2">Approving or removing this post will automatically resolve these reports.</p>
 	</div>
 </template>
 
