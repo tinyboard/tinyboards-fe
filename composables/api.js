7edce0b077f5d994823b8a61be6b2e08545459cd
kroner <kronusdev@protonmail.com>
2023-07-13 19:52:30 -0500

    asdf

--- composables/api.js
+++ composables/api.js
@@ -3,9 +3,7 @@
 export function useApi(path, options) {
   const authCookie = useCookie("token").value;
   const config = useRuntimeConfig();
-
-  console.log(config);
-
+  
   return useFetch(path, {
     baseURL: `${config.public.useHTTPS ? "https" : "http"}://${config.public.domain}/api/v1`,
     headers: {
