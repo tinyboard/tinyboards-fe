aee4d8132d601c358976e445a3ffa496eafb1809
kroner <kronusdev@protonmail.com>
2023-07-18 19:25:40 -0500

    remove a debug log

--- composables/api.js
+++ composables/api.js
@@ -4,8 +4,6 @@ export function useApi(path, options) {
   const authCookie = useCookie("token").value;
   const config = useRuntimeConfig();
   
-  console.log(config);
-
   return useFetch(path, {
     baseURL: `${config.public.use_https ? "https" : "http"}://${config.public.domain}/api/v1`,
     headers: {
