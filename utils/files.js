b6279805b35ed29873ecc6d4e59578b9e058a930
i-dont-know11 <129621627+i-dont-know11@users.noreply.github.com>
2023-08-29 16:59:29 +0200

    delete the main page

--- utils/files.js
+++ utils/files.js
@@ -1,14 +1,14 @@
-export const dataURLtoFile = dataURL => {
-	const type = dataURL.split(';')[0].slice(5);
+export const dataURLtoFile = (dataURL) => {
+  const type = dataURL.split(";")[0].slice(5);
 
-	const arr = dataURL.split(',');
-	const bstr = atob(arr[arr.length - 1]);
-	let n = bstr.length;
-	let u8arr = new Uint8Array(n);
+  const arr = dataURL.split(",");
+  const bstr = atob(arr[arr.length - 1]);
+  let n = bstr.length;
+  let u8arr = new Uint8Array(n);
 
-	while (n--) {
-		u8arr[n] = bstr.charCodeAt(n);
-	}
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
 
-	return new File([u8arr], `upload.${type.split("/")[1]}`, {type: type});
+  return new File([u8arr], `upload.${type.split("/")[1]}`, { type: type });
 };
