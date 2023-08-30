e39168e737cc825902b9a0e7f7287cb7174b591e
i-dont-know11 <129621627+i-dont-know11@users.noreply.github.com>
2023-08-30 23:24:27 +0200

    messages

--- layouts/inbox.vue
+++ layouts/inbox.vue
@@ -2,21 +2,18 @@
 	<main id="layout-inbox" class="flex flex-col md:h-screen pt-12 sm:pt-14">
 		<!-- Sub Navigation & Banner -->
 		<section class="flex flex-col md:hidden">
-			<NavigationNavbarSub :links="links" class="sm:order-first"/>
+			<NavigationNavbarSub :links="links" class="sm:order-first" />
 			<div class="order-first sm:order-last container mx-auto max-w-4xl grid grid-cols-12 sm:mt-16 sm:px-4 md:px-6">
 				<!-- Banner -->
-				<CardsBanner
-				class="col-span-full"
-				title="Inbox"
-				sub-title="Catch up on replies, mentions, and private messages"
-				image-url="https://forum.porpl.net/image/51bc2d71-6c70-4add-a7b0-03fed9024fb6.webp"
-				/>
+				<CardsBanner class="col-span-full" title="Inbox"
+					sub-title="Catch up on replies, mentions, and private messages"
+					image-url="https://forum.porpl.net/image/51bc2d71-6c70-4add-a7b0-03fed9024fb6.webp" />
 			</div>
 		</section>
 		<!-- Main Content -->
 		<section class="container mx-auto max-w-8xl h-full flex sm:p-4">
 			<div class="h-full w-56 hidden md:flex flex-shrink-0 flex-col gap-4 bg-transparent">
-<!-- 				<NuxtLink to="/inbox/messages/new" class="flex items-center button primary mr-4">
+				<!-- 				<NuxtLink to="/inbox/messages/new" class="flex items-center button primary mr-4">
 					<svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
 						<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
 						<path d="M8 20l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4h4z"></path>
@@ -26,7 +23,8 @@
 					New message
 				</NuxtLink> -->
 				<button class="flex items-center button primary mr-4" disabled>
-					<svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+					<svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" viewBox="0 0 24 24" stroke-width="2"
+						stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
 						<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
 						<path d="M8 20l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4h4z"></path>
 						<path d="M13.5 6.5l4 4"></path>
@@ -37,10 +35,14 @@
 				<ul class="flex flex-col z-10 text-sm mr-[-1px] overflow-y-auto">
 					<li class="relative">
 						<NuxtLink to="/inbox" custom v-slot="{ href, navigate, isActive }">
-							<a :href="href" @click="navigate" class="flex items-center px-4 py-2.5 border border-r-0 rounded-l-md font-bold" :class="isActive ? 'text-gray-600 bg-gray-50 shadow-inner-white' : 'text-gray-400 hover:text-gray-600 border-transparent'">
-								<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-								   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-								   <path d="M3 20l1.3 -3.9a9 8 0 1 1 3.4 2.9l-4.7 1"></path>
+							<a :href="href" @click="navigate"
+								class="flex items-center px-4 py-2.5 border border-r-0 rounded-l-md font-bold"
+								:class="isActive ? 'text-gray-600 bg-gray-50 shadow-inner-white' : 'text-gray-400 hover:text-gray-600 border-transparent'">
+								<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2" viewBox="0 0 24 24"
+									stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+									stroke-linejoin="round">
+									<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+									<path d="M3 20l1.3 -3.9a9 8 0 1 1 3.4 2.9l-4.7 1"></path>
 								</svg>
 								Replies
 							</a>
@@ -48,81 +50,92 @@
 					</li>
 					<li class="relative">
 						<NuxtLink to="/inbox/mentions" custom v-slot="{ href, navigate, isActive }">
-							<a :href="href" @click="navigate" class="flex items-center px-4 py-2.5 border border-r-0 rounded-l-md font-bold" :class="isActive ? 'text-gray-600 bg-gray-50 shadow-inner-white' : 'text-gray-400 hover:text-gray-600 border-transparent'">
-								<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-								   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-								   <circle cx="12" cy="12" r="4"></circle>
-								   <path d="M16 12v1.5a2.5 2.5 0 0 0 5 0v-1.5a9 9 0 1 0 -5.5 8.28"></path>
+							<a :href="href" @click="navigate"
+								class="flex items-center px-4 py-2.5 border border-r-0 rounded-l-md font-bold"
+								:class="isActive ? 'text-gray-600 bg-gray-50 shadow-inner-white' : 'text-gray-400 hover:text-gray-600 border-transparent'">
+								<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2" viewBox="0 0 24 24"
+									stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+									stroke-linejoin="round">
+									<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+									<circle cx="12" cy="12" r="4"></circle>
+									<path d="M16 12v1.5a2.5 2.5 0 0 0 5 0v-1.5a9 9 0 1 0 -5.5 8.28"></path>
 								</svg>
 								Mentions
 							</a>
 						</NuxtLink>
 					</li>
-					<!-- <li class="relative">
+					<li class="relative">
 						<NuxtLink to="/inbox/messages" custom v-slot="{ href, navigate, isActive }">
-							<a :href="href" @click="navigate" class="flex items-center px-4 py-2.5 border border-r-0 rounded-l-md font-bold" :class="isActive ? 'text-gray-600 bg-gray-50 shadow-inner-white' : 'text-gray-400 hover:text-gray-600 border-transparent'">
-								<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-								   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-								   <path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10"></path>
-								   <path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2"></path>
+							<a :href="href" @click="navigate"
+								class="flex items-center px-4 py-2.5 border border-r-0 rounded-l-md font-bold"
+								:class="isActive ? 'text-gray-600 bg-gray-50 shadow-inner-white' : 'text-gray-400 hover:text-gray-600 border-transparent'">
+								<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2" viewBox="0 0 24 24"
+									stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+									stroke-linejoin="round">
+									<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+									<path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10"></path>
+									<path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2"></path>
 								</svg>
 								Messages
 							</a>
 						</NuxtLink>
-					</li> -->
-					<li class="relative">
-						<button class="w-full flex items-center px-4 py-2.5 border border-r-0 rounded-l-md font-bold text-gray-400 hover:text-gray-600 border-transparent" disabled>
-							<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-							   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-							   <path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10"></path>
-							   <path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2"></path>
+					</li>
+					<!--<li class="relative">
+						<button
+							class="w-full flex items-center px-4 py-2.5 border border-r-0 rounded-l-md font-bold text-gray-400 hover:text-gray-600 border-transparent">
+							<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2" viewBox="0 0 24 24"
+								stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+								stroke-linejoin="round">
+								<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+								<path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10"></path>
+								<path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2"></path>
 							</svg>
 							Messages
 						</button>
-					</li>
+					</li> -->
 				</ul>
 			</div>
 			<div class="h-full w-full flex flex-col gap-6 md:p-4 bg-gray-50 md:border md:rounded-md md:shadow-inner-white">
 				<!-- Messages -->
-				<slot/>
+				<slot />
 			</div>
 		</section>
 	</main>
 </template>
 
 <script setup>
-	import { useSiteStore } from '@/stores/StoreSite';
-	const route = useRoute();
-	const site = useSiteStore();
+import { useSiteStore } from '@/stores/StoreSite';
+const route = useRoute();
+const site = useSiteStore();
 
-	useHead({
-		bodyAttrs: {
-	      class: 'md:overflow-hidden'
-	    },
-		title: `${site.name} | ${route.meta.title ?? 'inbox'}`,
-		meta: [{ property: 'og:title',
-			content: `${site.name} | ${route.meta.title}`
-		}]
-	});
+useHead({
+	bodyAttrs: {
+		class: 'md:overflow-hidden'
+	},
+	title: `${site.name} | ${route.meta.title ?? 'inbox'}`,
+	meta: [{
+		property: 'og:title',
+		content: `${site.name} | ${route.meta.title}`
+	}]
+});
 
-	const links = [
+const links = [
 	{ name: 'Replies', href: '/inbox' },
 	{ name: 'Mentions', href: '/inbox/mentions' },
-	// { name: 'Messages', href: '/inbox/messages' },
-	];
+	{ name: 'Messages', href: '/inbox/messages' },
+];
 </script>
 
 <style scoped>
-	.arrow__right::before {
-		content: "";
-		position: absolute;
-		top: 11px;
-		right: -8px;
-		display: block;
-		width: 16px;
-		height: 16px;
-		transform: rotate(45deg);
-		box-sizing: content-box;
-		@apply border-l border-b bg-gray-50;
-	}
-</style>
+.arrow__right::before {
+	content: "";
+	position: absolute;
+	top: 11px;
+	right: -8px;
+	display: block;
+	width: 16px;
+	height: 16px;
+	transform: rotate(45deg);
+	box-sizing: content-box;
+	@apply border-l border-b bg-gray-50;
+}</style>
--- pages/inbox/messages.vue
+++ pages/inbox/messages.vue
@@ -1,6 +1,7 @@
 <template>
 	<NuxtLayout name="inbox">
-		<div class="flex flex-col h-full bg-white sm:shadow-inner-white sm:rounded-md border-b sm:border sm:overflow-hidden">
+		<div
+			class="flex flex-col h-full bg-white sm:shadow-inner-white sm:rounded-md border-b sm:border sm:overflow-hidden">
 			<!-- Page Heading & Description -->
 			<div class="flex items-center p-4 border-b">
 				<h2 class="text-lg font-bold leading-6 text-gray-900 capitalize">
@@ -13,85 +14,133 @@
 					<button class="ml-4 button button-sm gray" @click="markRead" :disabled="unreadCount === 0 || isLoading">
 						&#10003; Mark all read
 					</button>
-					<button v-if="route.params.id" class="ml-2 flex items-center button button-sm gray" @click="markRead" :disabled="unreadCount === 0 || isLoading">
-						<svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3 mr-1" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-						   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-						   <line x1="4" y1="7" x2="20" y2="7"></line>
-						   <line x1="10" y1="11" x2="10" y2="17"></line>
-						   <line x1="14" y1="11" x2="14" y2="17"></line>
-						   <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
-						   <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
+					<button v-if="route.params.id" class="ml-2 flex items-center button button-sm gray" @click="markRead"
+						:disabled="unreadCount === 0 || isLoading">
+						<svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3 mr-1" viewBox="0 0 24 24" stroke-width="2"
+							stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+							<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+							<line x1="4" y1="7" x2="20" y2="7"></line>
+							<line x1="10" y1="11" x2="10" y2="17"></line>
+							<line x1="14" y1="11" x2="14" y2="17"></line>
+							<path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
+							<path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
 						</svg>
 						Delete
 					</button>
 				</div>
 			</div>
 			<div class="grid grid-cols-12 h-full sm:overflow-hidden">
-				<ul class="col-span-4 md:col-span-4 h-full divide-y md:border-r sm:overflow-y-auto">
-					<li v-for="(conversation, i) in conversations.messages" :key="i">
-						<NuxtLink :to="`/inbox/messages/${conversation.private_message.chat_id}`" custom v-slot="{ href, navigate, isActive }">
-							<a @click="navigate" class="flex p-2.5 hover:bg-gray-100 shadow-inner-white" :class="{'bg-gray-100':isActive}">
-								<img loading="lazy" :src="conversation.creator.avatar" alt="avatar" class="object-cover w-16 h-16 sm:p-0.5 sm:border bg-white hover:bg-gray-200 hover:border-transparent"/>
-								<span class="ml-2 text-gray-900">
-									<strong class="text-sm">
-										{{ userStore.user.name === conversation.creator.name ? conversation.recipient.name : conversation.creator.name }}
-									</strong>
-									<p class="text-xs text-gray-600 font-medium">
+				<ul class="col-span-12 md:col-span-12 h-full divide-y md:border-r sm:overflow-y-auto">
+					<li v-for="(conversation, i) in conversations" :key="i">
+						<!--<NuxtLink :to="`/inbox/messages`" custom v-slot="{ href, navigate, isActive }">
+							<a @click="navigate" class="flex p-2.5 hover:bg-gray-100 shadow-inner-white"
+								:class="{ 'bg-gray-100': isActive }">-->
+						<div class="flex p-2.5 hover:bg-gray-100 shadow-inner-white"
+							:class="conversation.notif?.read ? 'bg-gray-100' : 'bg-blue-100'">
+
+							<img loading="lazy" :src="conversation.creator.avatar" alt="avatar"
+								class="object-cover w-16 h-16 sm:p-0.5 sm:border bg-white hover:bg-gray-200 hover:border-transparent" />
+							<span class="ml-2 text-gray-900">
+								<strong class="text-sm"
+									:class="conversation.creator.is_admin ? 'text-red-700' : 'text-gray-900'">
+									{{ userStore.user.name === conversation.creator.name ?
+										conversation.recipient_user.name : conversation.creator.name }}
+
+									<span v-if="conversation.creator.is_admin" class="ml-1 badge badge-red">
+										{{ conversation.creator.id == 1 ? "System" : "Admin" }}
+									</span>
+								</strong>
+								<!--<p class="text-xs text-gray-600 font-medium">
 										{{ conversation.private_message.subject }}
-									</p>
-									<div v-html="conversation.private_message.body"></div>
-								</span>
-							</a>
-						</NuxtLink>
+									</p>-->
+								<div v-html="conversation.message.body_html"></div>
+							</span>
+						</div>
+						<!--</a>
+						</NuxtLink>-->
 					</li>
 				</ul>
 				<!-- Nested Page -->
-				<NuxtPage v-if="route.path !== '/inbox/messages'" class="col-span-8 overflow-hidden"/>
+				<NuxtPage v-if="route.path !== '/inbox/messages'" class="col-span-8 overflow-hidden" />
 				<!-- Empty State -->
-				<div v-else class="col-span-8 flex flex-col flex-grow items-center justify-center p-4 text-center text-gray-500 overflow-hidden">
+				<!--<div v-else
+					class="col-span-8 flex flex-col flex-grow items-center justify-center p-4 text-center text-gray-500 overflow-hidden">
 					<p>
 						<span class="font-medium">
 							There are no messages in your inbox...
 						</span>
-						<br/>
+						<br />
 						how about composing a message
-						<div class="mt-4 flex justify-center">
-							<NuxtLink to="/inbox/messages/new" class="flex items-center button gray">
-								<svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-								   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-								   <path d="M8 20l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4h4z"></path>
-								   <path d="M13.5 6.5l4 4"></path>
-								   <path d="M16 18h4m-2 -2v4"></path>
-								</svg>
-								Message someone
-							</NuxtLink>
-						</div>
+					<div class="mt-4 flex justify-center">
+						<NuxtLink to="/inbox/messages/new" class="flex items-center button gray">
+							<svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" viewBox="0 0 24 24"
+								stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+								stroke-linejoin="round">
+								<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+								<path d="M8 20l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4h4z"></path>
+								<path d="M13.5 6.5l4 4"></path>
+								<path d="M16 18h4m-2 -2v4"></path>
+							</svg>
+							Message someone
+						</NuxtLink>
+					</div>
 					</p>
-				</div>
+				</div>-->
 			</div>
 		</div>
 	</NuxtLayout>
 </template>
 
 <script setup>
-	// import { baseURL } from '@/server/constants';
-	import { useApi } from "@/composables/api";
-	import { useLoggedInUser } from "@/stores/StoreAuth";
+// import { baseURL } from '@/server/constants';
+import { useApi } from "@/composables/api";
+import { useLoggedInUser } from "@/stores/StoreAuth";
+import { ref } from 'vue';
 
-	definePageMeta({
-		'hasAuthRequired': true
-	});
+definePageMeta({
+	'hasAuthRequired': true
+});
 
-	const route = useRoute();
+const route = useRoute();
 
-	const authCookie = useCookie("token").value;
+const authCookie = useCookie("token").value;
 
-	const userStore = useLoggedInUser();
+const userStore = useLoggedInUser();
 
-	const { data: conversations, pending, error, refresh } = await useApi('/messages', {
-		query: {
-			limit: 25,
-			page: 1,
-		}
+const isLoading = ref(true);
+
+const { data, pending, error, refresh } = await useApi('/messages', {
+	query: {
+		limit: 25,
+		page: 1,
+	}
+})
+
+isLoading.value = false;
+
+const conversations = data.value.messages;
+const unreadCount = ref(data.value.unread);
+
+const markRead = () => {
+	isLoading.value = true;
+	useApi(`/notifications/messages/mark_read`, {
+		method: "post",
+		body: {}
 	})
+		.then(({ data, error }) => {
+			if (!error.value) {
+				unreadCount.value = 0;
+				// Show success toast.
+				toast.addNotification({ header: 'Marked all read', message: `All messages marked as read.`, type: 'success' });
+			} else {
+				// Show error toast.
+				toast.addNotification({ header: 'Failed to mark all read', message: 'Please try again.', type: 'error' });
+				// Log the error.
+				console.error(error.value);
+			}
+		})
+		.finally(() => {
+			isLoading.value = false;
+		});
+};
 </script>
