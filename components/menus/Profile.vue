66f62a8a3ce5ec502c6edf3b21a6cc6f2a11c09b
a <135359235+githubcom3@users.noreply.github.com>
2023-10-09 16:46:07 -0400

    Improve ordering
    also add comments link

--- components/menus/Profile.vue
+++ components/menus/Profile.vue
@@ -59,6 +59,29 @@
               <span>My Profile</span>
             </NuxtLink>
           </MenuItem>
+          <!-- Posts Link -->
+          <MenuItem v-slot="{ active, close }">
+            <NuxtLink :to="`/@${user.name}/posts`" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700','group flex items-center w-full px-4 py-1.5']" @click="close">
+              <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                 <path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4"></path>
+                 <line x1="13.5" y1="6.5" x2="17.5" y2="10.5"></line>
+              </svg>
+              <span>Posts</span>
+            </NuxtLink>
+          </MenuItem>
+          <!-- Comments Link -->
+          <MenuItem v-slot="{ active, close }">
+            <NuxtLink :to="`/@${user.name}/comments`" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700','group flex items-center w-full px-4 py-1.5']" @click="close">
+              <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                 <path d="M3 20l1.3 -3.9a9 8 0 1 1 3.4 2.9l-4.7 1"></path>
+              </svg>
+              <span>Comments</span>
+            </NuxtLink>
+          </MenuItem>
+        </div>
+        <div class="py-2 text-sm">
           <!-- Inbox Link -->
           <MenuItem v-slot="{ active, close }">
             <NuxtLink to="/inbox" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700','group flex items-center w-full px-4 py-1.5']" @click="close">
@@ -71,17 +94,8 @@
               <strong v-if="unread > 0" class="text-primary">&nbsp;{{ unread }}</strong>
             </NuxtLink>
           </MenuItem>
-          <!-- Posts Link -->
-          <MenuItem v-slot="{ active, close }">
-            <NuxtLink :to="`/@${user.name}/posts`" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700','group flex items-center w-full px-4 py-1.5']" @click="close">
-              <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                 <path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4"></path>
-                 <line x1="13.5" y1="6.5" x2="17.5" y2="10.5"></line>
-              </svg>
-              <span>Posts</span>
-            </NuxtLink>
-          </MenuItem>
+        </div>
+        <div class="py-2 text-sm">
           <MenuItem v-slot="{ active, close }">
             <NuxtLink to="/settings/profile" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700','group flex items-center w-full px-4 py-1.5']" @click="close">
               <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
