102402d277dab86b23558fbd3c57fe5f94055de4
kronerdev <kronusdev@protonmail.com>
2025-11-14 19:02:10 -0600

    feat: add image icon for uploaded images
    - Add green image icon for posts with uploaded images
    - Improve visual distinction between images, videos, and links
    - Use proper icon hierarchy: video > image > link

--- components/cards/Post.vue
+++ components/cards/Post.vue
@@ -145,6 +145,17 @@
                 <rect x="3" y="6" width="12" height="12" rx="2"></rect>
               </svg>
             </span>
+            <!-- Image Icon -->
+            <span v-else-if="hasImage || hasUploadedImage" title="Post contains an image">
+              <svg xmlns="http://www.w3.org/2000/svg" class="text-green-500 w-4 h-4" viewBox="0 0 24 24" stroke-width="2"
+                stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M15 8h.01"></path>
+                <rect x="4" y="4" width="16" height="16" rx="3"></rect>
+                <path d="M4 15l4 -4a3 5 0 0 1 3 0l5 5"></path>
+                <path d="M14 14l1 -1a3 5 0 0 1 3 0l2 2"></path>
+              </svg>
+            </span>
             <!-- Link Icon -->
             <span v-else-if="!!post.url" title="Post contains a link">
               <svg xmlns="http://www.w3.org/2000/svg" class="text-gray-500 w-4 h-4" viewBox="0 0 24 24" stroke-width="2"
